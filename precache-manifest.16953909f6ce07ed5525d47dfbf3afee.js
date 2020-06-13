2ec5e25860ecd0b1e2d3838a69d62f43e1dc57cf
Jack-cool <1263215592@qq.com>
2020-06-13 08:13:44 +0000

    Deploying to gh-pages from master fcd3eee5121fd53c0fa31bc2058d6e6d0ffdfe8e

---
+++ precache-manifest.16953909f6ce07ed5525d47dfbf3afee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43cd479a69ea36abe445fa7aa6e497bf",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "72a4b07246c7dcd39e37",
+    "url": "/github-actions-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4bec14b9d3df75b0e44",
+    "url": "/github-actions-demo/static/js/2.7a9d9c59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-actions-demo/static/js/2.7a9d9c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a4b07246c7dcd39e37",
+    "url": "/github-actions-demo/static/js/main.5c63a904.chunk.js"
+  },
+  {
+    "revision": "67c1217f3744b7668bfa",
+    "url": "/github-actions-demo/static/js/runtime-main.8de8569e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
